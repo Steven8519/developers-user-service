e972c0589f594dcd7baf82f099bb4bb0fca3121f
Steven8519 <engineers2526@gmail.com>
2021-09-20 03:18:15 -0500

    test

--- src/test/java/com/dev/social/core/developersuserservice/DevelopersUserServiceApplicationTests.java
+++ src/test/java/com/dev/social/core/developersuserservice/DevelopersUserServiceApplicationTests.java
@@ -3,11 +3,6 @@ package com.dev.social.core.developersuserservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DevelopersUserServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
