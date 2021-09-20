a085ef7740d93c73f63b10a55ff53a37f748a87f
Steven8519 <engineers2526@gmail.com>
2021-09-20 03:07:26 -0500

    Initial commit

---
+++ src/main/java/com/dev/social/core/developersuserservice/DevelopersUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dev.social.core.developersuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopersUserServiceApplication.class, args);
+    }
+
+}
